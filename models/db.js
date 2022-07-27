0126d0a6464861122cff9389761d09f647a705bc
jyliao369 <jyliao369@gmail.com>
2022-07-27 10:34:27 -0400

    init

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
+const dbConfig = require("../config/db.config");
 
 var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
 });
 
 module.exports = connection;
