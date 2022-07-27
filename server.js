4092830fd02b736a2cfd6fd6c478db1e470c180c
jyliao369 <jyliao369@gmail.com>
2022-07-26 21:16:06 -0400

    init

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Valerie Call on me." });
 });
 
-require("./app/routes/tutorial.routes.js")(app);
+// require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
