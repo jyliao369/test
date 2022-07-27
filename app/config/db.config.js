5372fbac3925fcde7ea25d61cd7a19b0c63fb75e
jyliao369 <jyliao369@gmail.com>
2022-07-26 21:11:38 -0400

    init

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b4f0d2471a57c3",
+  PASSWORD: "328233e9",
+  DB: "heroku_24d76449a5017c1",
 };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
@@ -14,11 +14,13 @@ app.use(cors(corsOptions));
 app.use(express.json()); /* bodyParser.json() is deprecated */
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+app.use(
+  express.urlencoded({ extended: true })
+); /* bodyParser.urlencoded() is deprecated */
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Valerie Call on me." });
 });
 
 require("./app/routes/tutorial.routes.js")(app);
