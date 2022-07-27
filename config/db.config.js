e2c5d8b836324674da5a43b150966bd1891660c3
jyliao369 <jyliao369@gmail.com>
2022-07-27 11:21:32 -0400

    init

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  HOST: "us-cdbr-east-06.cleardb.net",
-  USER: "b4f0d2471a57c3",
-  PASSWORD: "328233e9",
-  DB: "heroku_24d76449a5017c1",
-};
+// module.exports = {
+//   HOST: "us-cdbr-east-06.cleardb.net",
+//   USER: "b4f0d2471a57c3",
+//   PASSWORD: "328233e9",
+//   DB: "heroku_24d76449a5017c1",
+// };
