b858fefaaca38c67745fd085c326f9b1cae34c0a
jyliao369 <jyliao369@gmail.com>
2022-07-27 15:27:10 -0400

    init

--- server.js
+++ server.js
@@ -21,14 +21,6 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-});
-
 // parse requests of content-type - application/json
 app.use(express.json());
 
