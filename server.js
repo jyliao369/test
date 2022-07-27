85a1db11fa15b9e59edef9a5c8506f253d5eff63
jyliao369 <jyliao369@gmail.com>
2022-07-27 11:03:35 -0400

    init

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const app = express();
 
@@ -11,12 +11,12 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // THIS IS THE MYSQL CONNECTION
-// const connection = mysql.createConnection({
-//   HOST: "us-cdbr-east-06.cleardb.net",
-//   USER: "b4f0d2471a57c3",
-//   PASSWORD: "328233e9",
-//   DB: "heroku_24d76449a5017c1",
-// });
+const db = mysql.createConnection({
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b4f0d2471a57c3",
+  PASSWORD: "328233e9",
+  DB: "heroku_24d76449a5017c1",
+});
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -26,7 +26,18 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Valerie Call on me." });
+  db.query(
+    `SELECT * FROM heroku_24d76449a5017c1.posts_table`,
+    [],
+    (err, result) => {
+      if (err) {
+        res.json({ message: err });
+      } else {
+        // res.json({ message: "Valerie Call on me." });
+        res.json(result);
+      }
+    }
+  );
 });
 
 app.get("/hey", (req, res) => {
